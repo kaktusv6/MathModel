7e42c8f42c56f1e3743e555526e721a534922650
Vasiliy <kaktusv6@gmail.com>
2016-12-14 01:45:04 +1100

    Добавил скрипт для парсинга объекта в JSON.

---
+++ app/js/index.js
@@ -0,0 +1,22 @@
+$(document).ready(function () {
+	$('.but-submit').click(function(event) {
+		var objModel = {
+			period: 0,
+			countCities: 0,
+			month: '',
+			price: 0,
+			fund: 0,
+			cities: []
+		}
+
+		objModel.period = $('#period-input').val();
+		objModel.countCities = $('#cities-input').val();
+		objModel.month = $('#mounth-input').val();
+		objModel.price = $('#price-input').val();
+		objModel.fund = $('#fund-input').val();
+
+		var strJson = JSON.stringify(objModel);
+
+		alert(strJson);
+	});
+});
